bf013fab7e3aaeac635c3c7f25d7179a567423d3
Hedwika <pajerova.hedvika@gmail.com>
2021-04-23 17:03:07 +0200

    Lekce 8 - autopůjčovna

---
+++ venv/Scripts/pujcovna/katalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="katalog")
+]
+
+urlpatterns = [
+    path('seznam/', views.SeznamView.as_view(), name='seznam')
+]
