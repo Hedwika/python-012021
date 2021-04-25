e0f5ecb2b46fc27a0764a5acf7438107d49b17c9
Hedwika <pajerova.hedvika@gmail.com>
2021-04-25 19:27:10 +0200

    Ukoly z 9. lekce

--- venv/Scripts/pujcovna/katalog/models.py
+++ venv/Scripts/pujcovna/katalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+    registracni_znacka = models.CharField(max_length=6)
+    znacka_a_typ = models.CharField(max_length=100)
+    pocet_najetych_km = models.IntegerField()
+    datum_posledni_technicke = models.DateField()
+
+class Zakaznik(models.Model):
+    jmeno = models.CharField(max_length=100)
+    prijmeni = models.CharField(max_length=100)
+    cislo_ridicskeho_prukazu = models.CharField(max_length=15)
+    datum_narozeni = models.DateField()
+
+class Vypujcka(models.Model):
+    zacatek = models.DateTimeField()
+    konec = models.DateTimeField()
+    cena = models.IntegerField()
