c02f39bc174e1837b993e5b70712fb198e679f68
Hedwika <pajerova.hedvika@gmail.com>
2021-04-25 18:32:48 +0200

    pujcovna - opravene paths

--- venv/Scripts/pujcovna/katalog/urls.py
+++ venv/Scripts/pujcovna/katalog/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="katalog")
-]
-
-urlpatterns = [
+    path('', views.IndexView.as_view(), name="katalog"),
     path('seznam/', views.SeznamView.as_view(), name='seznam')
 ]
